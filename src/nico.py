3a3a565676b5b8d1ec6adaba705c2b5e924b8d6e
Owo <oceane.cras@isen-ouest.yncrea.fr>
2024-01-25 11:53:56 +0100

    nico's emotions ok

--- src/nico.py
+++ src/nico.py
@@ -2,12 +2,13 @@ import pygame
 import sys
 
 class Nico():
-    def __init__(self, energy, hunger, hygiene, social, isAsleep):
+    def __init__(self, energy, hunger, hygiene, social, isAsleep, emotion):
         self.energy = energy
         self.hunger = hunger
         self.hygiene = hygiene
         self.social = social
         self.isAsleep = isAsleep
+        self.emotion = emotion
     
     def getTired(self):
         coef = 2 if self.isAsleep else -1
@@ -23,21 +24,21 @@ class Nico():
         # respective thresholds are 0, 1, 2. 0 being the lowest, 2 being the highest.
 
         # get tired
-        if self.energy > 0: self.energy -= 0.3
+        if self.energy > 0.3: self.energy -= 0.3
         # get hungry
         if self.hunger > 0: self.hunger -= 0.5
         # get dirty (coming feature)
         # want to play (coming feature)
 
-    def feelEmotion(self, energy, hunger, hygiene, social):
+    def feelEmotion(self):
         # if hunger or energy =< 1, get angry
-        if energy or hunger <= 1:
+        if self.energy <= 1 or self.hunger <= 1:
             emotion = 'angry'
         # if social or hygiene =<1 , get sad
-        elif hygiene or social <= 1:
+        elif self.hygiene <=1 or self.social <= 1:
             emotion = 'sad'
         # get happy if all needs are met ^_^
-        elif energy and hunger and hygiene and social >=2:
+        elif self.energy >=2 and self.hunger >=2 and self.hygiene >= 2 and self.social >=2:
             emotion = 'happy'
         else:
             emotion = 'neutral'
@@ -74,12 +75,12 @@ class Nico():
         # play (add social to the bar punctually)
         else:
             social += 1
-            print("Nico and you played together.")
+            print("Nico and you played together, you both had a lot of fun !")
 
 def main():
     pygame.init()
 
-    nico = Nico(3,3,3,3,False)
+    nico = Nico(3,3,3,3,False,'happy')
 
     clock = pygame.time.Clock()
     fps = 60
@@ -104,6 +105,7 @@ def main():
 
         if time_elapsed >= 1000:
             nico.live()
+            nico.feelEmotion()
             time_elapsed = 0
 
         # Affichage du résultat
@@ -111,8 +113,10 @@ def main():
         font = pygame.font.Font(None, 36)
         text_energy = font.render(f"Energy: {round(nico.energy, 2)}/3", True, (0, 0, 0))
         text_hunger = font.render(f"Hunger: {round(nico.hunger, 2)}/3", True, (0, 0, 0))
+        text_emotion = font.render(f"Emotion: {nico.feelEmotion()}", True, (0, 0, 0))
         screen.blit(text_energy, (0, 0))
         screen.blit(text_hunger, (0, 25))
+        screen.blit(text_emotion, (0, 50))
 
         pygame.display.flip()
 
