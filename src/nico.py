b9431309a074fc6f19db1bba66bb264db6397aad
Owo <oceane.cras@isen-ouest.yncrea.fr>
2024-01-30 10:24:27 +0100

    animations done :)

--- src/nico.py
+++ src/nico.py
@@ -3,21 +3,25 @@ from button import Button
 import sys
 #import game
 
-class Nico():
-    def __init__(self, energy, hunger, hygiene, social, isAsleep, emotion, img0, img1):
+class Nico(pygame.sprite.Sprite):
+    def __init__(self, energy, hunger, hygiene, social, isAsleep, emotion):
         self.energy = energy
         self.hunger = hunger
         self.hygiene = hygiene
         self.social = social
         self.isAsleep = isAsleep
         self.emotion = emotion
-        self.img0 = "assets/nico_"+self.emotion+"0.png"
-        self.img1 = "assets/nico_"+self.emotion+"1.png"
+        # self.img0 = "assets/nico_"+self.emotion+"0.png"
+        # self.img1 = "assets/nico_"+self.emotion+"1.png"
+        
+        # initialize current emotion's frames
+        for _ in range(2):
+            self.current_frames = ["assets/nico_"+self.emotion+str(_)+".png","assets/nico_"+self.emotion+str(_)+".png"]
 
     def live(self):
         """
         slowly decrease needs (energy, hunger, hygiene, social)
-        manage the sleep cycle of Nico
+        manage the sleep cycle of Nico, set frames depending on his emotion
         """
         # maximum of each need is 3
         # respective thresholds are 0, 1, 2, 3. 0 being the lowest, 3 being the highest.
@@ -41,10 +45,26 @@ class Nico():
         if self.hygiene > 0:
             self.hygiene -= coef * 0.1
 
-        # want to play (coming feature)
+        # want to play
         coef = 0.25 if self.isAsleep else 1
         if self.social > 0:
             self.social -= coef * 0.3
+
+        # get all sprites animation
+        frames = {}
+        lst_states = ['neutral', 'happy', 'angry', 'sad', 'sleep']
+        for state in lst_states:
+            state_frame = []
+            for _ in range(2):
+                state_frame.append("assets/nico_"+state+str(_)+".png")
+            frames[state] = state_frame        
+
+        # update image depending on emotion
+        if self.isAsleep:
+            self.current_frames = frames['sleep']
+        else:
+            self.current_frames = frames[self.emotion]
+
 
     def feelEmotion(self):
         """
@@ -53,9 +73,11 @@ class Nico():
         # if hunger or energy =< 1, get angry
         if self.energy <= 1 or self.hunger <= 1:
             emotion = 'angry'
+
         # if social or hygiene =<1 , get sad
         elif self.hygiene <=1 or self.social <= 1:
             emotion = 'sad'
+
         # get happy if all needs are met ^_^
         elif self.energy >=2 and self.hunger >=2 and self.hygiene >= 2 and self.social >=2:
             emotion = 'happy'
@@ -104,36 +126,26 @@ class Nico():
 def main():
     pygame.init()
 
-    nico = Nico(3,3,3,3,False,'happy','happy','happy')
+    nico = Nico(3,3,3,3,False,'happy')
 
     clock = pygame.time.Clock()
     fps = 60
     time_elapsed = 0
-    # decrementation interval in ms
-    # decrement_interval = 500
 
     # testing window creation
     screen = pygame.display.set_mode((400, 300))
     pygame.display.set_caption("Nico's behaviour testing")
     screen.fill((255,255,255))
-    surf = pygame.Surface((200, 150)) 
+    surf = pygame.Surface((700, 700)) 
     surf.fill((255,255,255)) # white rectangle
     font = pygame.font.Font(None, 36)
 
-    image0 = pygame.image.load(nico.img0)
-
-    # button creation
-
     # main loop
     while True:
-        # for event in pygame.event.get():
-        #     if event.type == pygame.QUIT:
-        #         pygame.quit()
-        #         sys.exit()
-        #     if event.type == pygame.MOUSEBUTTONDOWN:
-        #         # nico.feed(1)
-        #         # nico.clean()
-        #         # nico.play()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
         # update time
         dt = clock.tick(fps)
@@ -144,8 +156,16 @@ def main():
             nico.emotion = nico.feelEmotion()
             time_elapsed = 0
 
-        # display results on window
+        screen.fill((255, 255, 255))  # Efface l'écran
+        
+        # animations
+        if time_elapsed >= 250:
+            frame_surface = pygame.image.load(nico.current_frames[1])
+        else:
+            frame_surface = pygame.image.load(nico.current_frames[0])
         screen.blit(surf, (0, 0))
+        screen.blit(frame_surface, (150, 150))
+
         text_energy = font.render(f"Energy: {round(nico.energy, 2)}/3", True, (0, 0, 0))
         text_hunger = font.render(f"Hunger: {round(nico.hunger, 2)}/3", True, (0, 0, 0))
         text_hygiene = font.render(f"Hygiene: {round(nico.hygiene, 2)}/3", True, (0, 0, 0))
@@ -158,7 +178,6 @@ def main():
         screen.blit(text_social, (0, 75))
         screen.blit(text_sleep, (0, 100))
         screen.blit(text_emotion, (0, 125))
-        screen.blit(image0, (150,150))
         pygame.display.flip()
 
     # Quitter Pygame
